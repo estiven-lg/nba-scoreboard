2cd79d6fb0be1b56846cb3cd69b1b721beefff59
estiven-lg <estivenlaferre@gmail.com>
2025-08-23 19:52:23 -0600

    ...

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Scoreboard } from './scoreboard/scoreboard';
+import { ControlPanel } from './control-panel/control-panel';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'scoreboard', pathMatch: 'full' },
+    { path: 'scoreboard', component: Scoreboard },
+    { path: 'control-panel', component: ControlPanel },
+    { path: '**', redirectTo: 'scoreboard' }
+];
