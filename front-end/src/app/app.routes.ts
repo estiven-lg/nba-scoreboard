f33fb7b9ee5b8c13f3721c1d769575e3d2ed04a4
dannyflores <danny.flores@grupopit.com>
2025-08-25 02:01:19 -0600

    Controles ScoreBoard

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { Scoreboard } from './scoreboard/scoreboard';
-import { ControlPanel } from './control-panel/control-panel';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'scoreboard', pathMatch: 'full' },
     { path: 'scoreboard', component: Scoreboard },
-    { path: 'control-panel', component: ControlPanel },
+    { path: 'control-panel', component: ControlPanelComponent },
     { path: '**', redirectTo: 'scoreboard' }
 ];
--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Scoreboard } from './scoreboard/scoreboard';
+import { ControlPanelComponent } from './control-panel/control-panel.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Scoreboard],
+  imports: [RouterOutlet, Scoreboard, ControlPanelComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
