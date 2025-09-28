bdb865ade619b9ee183a7cc1f7c335eff8006e9a
estiven-lg <estivenlaferre@gmail.com>
2025-09-28 16:12:33 -0600

    nav bar

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('nba-scoreboard');
+  title = 'nba-scoreboard';
 }
